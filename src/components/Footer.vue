9db1eb0b74c6567ac84772cd5c30583679902836
Jeden1102 <bum123@o2.pl>
2022-05-07 23:04:55 +0200

    hotfix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center flex bg-primary-gray text-white items-center justify-center space-x-2">
-        Made by <a href="https://github.com/Jeden1102">Jeden1102</a> with <img class="h-6 w-6" src="/src/assets/img/heart.png" alt="">
+        Made by <a href="https://github.com/Jeden1102">Jeden1102</a> with  <img class="h-6 w-6" src="/src/assets/img/heart.png" alt="">
     </div>
 </template>
 
