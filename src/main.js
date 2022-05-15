fd47f4ada3f16e9a9f4014eedc6ff23cc87ae73b
Jeden1102 <bum123@o2.pl>
2022-05-15 23:45:06 +0200

    test

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import router from './router'
-import VueCookies from 'vue-cookies'
-import v3ImgPreview from 'v3-img-preview'
-createApp(App).use(router,VueCookies,v3ImgPreview).mount('#app')
